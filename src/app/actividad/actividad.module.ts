16c1f54a148399aba10fd259e421a88b596044a8
Kieran <K-Embee@protonmail.com>
2019-11-12 11:21:28 +0100

    Colores, finalizando parte visual de actividad

--- src/app/actividad/actividad.module.ts
+++ src/app/actividad/actividad.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { ActividadPage } from './actividad.page';
+import {ComponentsModule} from '../components.module';
 
 const routes: Routes = [
   {
@@ -17,6 +18,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [ActividadPage]
