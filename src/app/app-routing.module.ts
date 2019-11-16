d9d078f384ad560007a4bf28b8d4d02a82aabb15
Kieran <K-Embee@protonmail.com>
2019-11-16 19:42:24 +0100

    Añadido crear actividad, chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ const routes: Routes = [
   { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },  { path: 'actividad', loadChildren: './actividad/actividad.module#ActividadPageModule' },
   { path: 'valoracion', loadChildren: './valoracion/valoracion.module#ValoracionPageModule' },
-  { path: 'masinfo', loadChildren: './actividad/masinfo/masinfo.module#MasinfoPageModule' }
+  { path: 'masinfo', loadChildren: './actividad/masinfo/masinfo.module#MasinfoPageModule' },
+  { path: 'chat', loadChildren: './actividad/chat/chat.module#ChatPageModule' },
+  { path: 'crear', loadChildren: './actividad/crear/crear.module#CrearPageModule' }
 
 ];
 @NgModule({
