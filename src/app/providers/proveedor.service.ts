ce2dec5691a35ac570442b42ef5f4a9680172896
Kieran <K-Embee@protonmail.com>
2019-12-16 17:28:54 +0100

    login

--- src/app/providers/proveedor.service.ts
+++ src/app/providers/proveedor.service.ts
@@ -7,27 +7,29 @@ export class ProveedorService {
 
   constructor(public http:HttpClient) { }
 
+  ip = "http://192.168.102.3:3000"
+
   obtenerActividades(): Observable<any>{
-    return this.http.get('http://localhost:3000/actividades');
+    return this.http.get(this.ip + '/actividades');
   }
 
   obtenerActividadesRecomendadas(id, esSocio): Observable<any> {
     var consulta="recomendadasVoluntario";
     if(esSocio)
       consulta="recomendadasSocio";
-    return this.http.get('http://localhost:3000/actividades/' + consulta + "?id=" + id);
+    return this.http.get(this.ip + '/actividades/' + consulta + "?id=" + id);
   }
 
   obtenerActividad(i): Observable<any>{
-    return this.http.get('http://localhost:3000/actividades?id='+ i);
+    return this.http.get(this.ip + '/actividades?id='+ i);
   }
 
   obtenerCategorias(): Observable<any>{
-    return this.http.get('http://localhost:3000/actividades/categorias');
+    return this.http.get(this.ip + '/actividades/categorias');
   }
 
   obtenerCategoriasActividad(i): Observable<any>{
-    return this.http.get('http://localhost:3000/actividades/categoriasDeActividad?id='+i);
+    return this.http.get(this.ip + '/actividades/categoriasDeActividad?id='+i);
   }
 
   apuntadoActividad(id_part, id_act, esSocio): Observable<any>{
@@ -35,7 +37,7 @@ export class ProveedorService {
     if(esSocio)
       consulta="apuntadoSocio";
 
-    return this.http.get('http://localhost:3000/actividades/' + consulta + '?id_part='+ id_part + '&id_act=' + id_act);
+    return this.http.get(this.ip + '/actividades/' + consulta + '?id_part='+ id_part + '&id_act=' + id_act);
   }
 
   apuntarse(postData, esSocio): Observable<any>{
@@ -49,7 +51,7 @@ export class ProveedorService {
       })
     }
     console.log(JSON.stringify(postData));
-    return this.http.post("http://localhost:3000/actividades/" + consulta, JSON.stringify(postData), httpOptions);
+    return this.http.post(this.ip + "actividades/" + consulta, JSON.stringify(postData), httpOptions);
   }
 
   desapuntarse(postData, esSocio): Observable<any>{
@@ -63,7 +65,17 @@ export class ProveedorService {
       })
     }
     console.log(JSON.stringify(postData));
-    return this.http.post("http://localhost:3000/actividades/" + consulta, JSON.stringify(postData), httpOptions);
+    return this.http.post(this.ip + '/actividades' + consulta, JSON.stringify(postData), httpOptions);
+  }
+
+  enviarLogin(postData): Observable<any>{// Http Options
+      let httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      }
+      console.log(JSON.stringify(postData));
+      return this.http.post(this.ip + '/usuarios/loginApp', JSON.stringify(postData), httpOptions);
   }
 
   misActividadesRealizadas(id, esSocio): Observable<any>{
@@ -71,13 +83,13 @@ export class ProveedorService {
     if(esSocio)
       consulta="misActividadesSocio";
 
-      return this.http.get('http://localhost:3000/actividades/' + consulta + '?id='+ id +  '&proximas=0');
+      return this.http.get(this.ip + '/actividades/' + consulta + '?id='+ id +  '&proximas=0');
   }
   misActividadesProximas(id, esSocio): Observable<any>{
     var consulta="misActividadesVoluntario";
     if(esSocio)
       consulta="misActividadesSocio";
 
-      return this.http.get('http://localhost:3000/actividades/' + consulta + '?id='+ id + '&proximas=1');
+      return this.http.get(this.ip + '/actividades/' + consulta + '?id='+ id + '&proximas=1');
     }
 }
